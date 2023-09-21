7a937332b99d50f66159bae89592376fd5cbb2a3
satvik vemulapalli <111590060+satvik-vs@users.noreply.github.com>
2023-09-21 14:34:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
-// Function to fetch AQI data from the API
-function fetchAQIData() {
-    const apiUrl = 'https://api.example.com/aqi'; // Replace with the actual API endpoint
-    const apiKey = 'YOUR_API_KEY'; // Replace with your actual API key
+const apiUrl = 'https://api.ambeedata.com/latest/by-lat-lng?lat=12&lng=77';
+const apiKey = '2c30af85321ece6e47fc671496cd06829795f8cba7ca2886a530ac38bbc75d2a';
 
-    // Include the API key as a query parameter in the URL
-    const urlWithApiKey = `${apiUrl}?apiKey=${apiKey}`;
+// Set up headers with your API key
+const headers = new Headers({
+    'x-api-key': apiKey,
+});
 
-    // Make an HTTP GET request to the API
-    fetch(urlWithApiKey)
+// Set up the request configuration
+const requestOptions = {
+    method: 'GET',
+    headers: headers,
+};
+
+// Function to fetch data and populate HTML elements
+function fetchDataAndDisplay() {
+    // Get a reference to the API data section
+    const apiDataSection = document.getElementById('api-data-section');
+
+    // Make the API request
+    fetch(apiUrl, requestOptions)
         .then(response => {
             if (!response.ok) {
                 throw new Error('Network response was not ok');
@@ -15,42 +26,31 @@ function fetchAQIData() {
             return response.json();
         })
         .then(data => {
-            // Assuming the API response contains AQI parameters and AQI information
-            const pm25 = data.stations[0].PM25;
-            const pm10 = data.stations[0].PM10;
-            const no2 = data.stations[0].NO2;
-            const co = data.stations[0].CO;
-            const o2 = data.stations[0].OZONE;
-
-            // Update the webpage with the specific AQI parameters
-            document.getElementById("pm25-value").textContent = `PM2.5: ${pm25}`;
-            document.getElementById("pm10-value").textContent = `PM10: ${pm10}`;
-            document.getElementById("no2-value").textContent = `NO2: ${no2}`;
-            document.getElementById("co-value").textContent = `CO: ${co}`;
-            document.getElementById("o2-value").textContent = `O2: ${o2}`;
-
-            // Assuming the API response also contains AQI information
-            const aqi = data.stations[0].AQI;
-            const pollutant = data.stations[0].aqiInfo.pollutant;
-            const concentration = data.stations[0].aqiInfo.concentration;
-            const category = data.stations[0].aqiInfo.category;
-
-            // Update AQI-related elements
-            document.getElementById("aqi-value").textContent = `AQI: ${aqi}`;
-            document.getElementById("pollutant-value").textContent = `Pollutant: ${pollutant}`;
-            document.getElementById("concentration-value").textContent = `Concentration: ${concentration}`;
-            document.getElementById("category-value").textContent = `Category: ${category}`;
+            // Display API data in HTML elements
+            document.getElementById('co-value').textContent = data.stations[0].CO;
+            document.getElementById('no2-value').textContent = data.stations[0].NO2;
+            document.getElementById('ozone-value').textContent = data.stations[0].OZONE;
+            document.getElementById('pm10-value').textContent = data.stations[0].PM10;
+            document.getElementById('pm25-value').textContent = data.stations[0].PM25;
+            document.getElementById('so2-value').textContent = data.stations[0].SO2;
+            document.getElementById('city-value').textContent = data.stations[0].city;
+            document.getElementById('country-code-value').textContent = data.stations[0].countryCode;
+            document.getElementById('division-value').textContent = data.stations[0].division;
+            document.getElementById('lat-value').textContent = data.stations[0].lat;
+            document.getElementById('lng-value').textContent = data.stations[0].lng;
+            document.getElementById('place-name-value').textContent = data.stations[0].placeName;
+            document.getElementById('postal-code-value').textContent = data.stations[0].postalCode;
+            document.getElementById('state-value').textContent = data.stations[0].state;
+            document.getElementById('updated-at-value').textContent = data.stations[0].updatedAt;
+            document.getElementById('aqi-value').textContent = data.stations[0].AQI;
+            document.getElementById('pollutant-value').textContent = data.stations[0].aqiInfo.pollutant;
+            document.getElementById('concentration-value').textContent = data.stations[0].aqiInfo.concentration;
+            document.getElementById('category-value').textContent = data.stations[0].aqiInfo.category;
         })
         .catch(error => {
             console.error('Error:', error);
         });
 }
 
-// Function to update AQI data initially and then at regular intervals (every 5 seconds)
-function updateAQIData() {
-    fetchAQIData();
-}
-
-// Call the function to update AQI data initially and then at regular intervals (every 5 seconds)
-updateAQIData();
-setInterval(updateAQIData, 5000);
+// Call the function to fetch data and populate HTML elements
+fetchDataAndDisplay();
