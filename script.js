f262cd6c065995064ac52a2ece5714d55e386960
satvik vemulapalli <111590060+satvik-vs@users.noreply.github.com>
2023-09-21 14:10:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,56 @@
-// Replace these mock values with actual API calls
-const mockAQIData = 75;
-const mockWQIData = 85;
+// Function to fetch AQI data from the API
+function fetchAQIData() {
+    const apiUrl = ' https://api.ambeedata.com/latest/by-lat-lng?lat=12&lng=77'; // Replace with the actual API endpoint
+    const apiKey = '2c30af85321ece6e47fc671496cd06829795f8cba7ca2886a530ac38bbc75d2a'; // Replace with your actual API key
 
-// Function to update AQI and WQI values on the webpage
-function updateData() {
-    document.getElementById("aqi-value").textContent = mockAQIData;
-    document.getElementById("wqi-value").textContent = mockWQIData;
+    // Include the API key as a query parameter in the URL
+    const urlWithApiKey = `${apiUrl}?apiKey=${apiKey}`;
+
+    // Make an HTTP GET request to the API
+    fetch(urlWithApiKey)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            // Assuming the API response contains AQI parameters like PM2.5, PM10, NO2, CO, and O2
+            const pm25 = data.stations[0].PM25;
+            const pm10 = data.stations[0].PM10;
+            const no2 = data.stations[0].NO2;
+            const co = data.stations[0].CO;
+            const o2 = data.stations[0].OZONE;
+
+            // Update the webpage with the specific AQI parameters
+            document.getElementById("pm25-value").textContent = `PM2.5: ${pm25}`;
+            document.getElementById("pm10-value").textContent = `PM10: ${pm10}`;
+            document.getElementById("no2-value").textContent = `NO2: ${no2}`;
+            document.getElementById("co-value").textContent = `CO: ${co}`;
+            document.getElementById("o2-value").textContent = `O2: ${o2}`;
+
+            // Assuming the API response also contains AQI information
+            const aqi = data.stations[0].AQI;
+            const pollutant = data.stations[0].aqiInfo.pollutant;
+            const concentration = data.stations[0].aqiInfo.concentration;
+            const category = data.stations[0].aqiInfo.category;
+
+            // Update AQI-related elements
+            document.getElementById("aqi-value").textContent = `AQI: ${aqi}`;
+            document.getElementById("pollutant-value").textContent = `Pollutant: ${pollutant}`;
+            document.getElementById("concentration-value").textContent = `Concentration: ${concentration}`;
+            document.getElementById("category-value").textContent = `Category: ${category}`;
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
 }
 
-// Simulate data updates every 5 seconds (for demonstration purposes)
-updateData();
-setInterval(updateData, 5000);
+// Function to update AQI data initially and then at regular intervals (every 5 seconds)
+function updateAQIData() {
+    fetchAQIData();
+}
+
+// Call the function to update AQI data initially and then at regular intervals (every 5 seconds)
+updateAQIData();
+setInterval(updateAQIData, 5000);
